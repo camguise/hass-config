14c0d8911e1c46e34ef0c522d64cca662fbb3d83
Campbell Guise <cam@guise.co.nz>
2019-03-18 14:46:55 +1300

    Added check to ensure script is run as root

--- pullConfig.sh
+++ pullConfig.sh
@@ -4,6 +4,9 @@ HASS_VERSION='0.89.1'
 HASS_DIR='/usr/local/docker/hass'
 LETSENCRYPT_DIR='/usr/local/docker/letsencrypt/config/etc/letsencrypt'
 
+# make sure we're running as root
+if (( `$ID -u` != 0 )); then { $ECHO "Sorry, must be root.  Exiting..."; exit; } fi
+
 cd "${HASS_DIR}"
 
 git pull
